4abd5a56e50b9ef0042fc1103dd33255e3cc5583
ksojin1 <109198812+ksojin1@users.noreply.github.com>
2023-07-27 04:43:44 +0900

    최종완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import Calendar from './Calendar';
 import "./css/App.scss";
 
 const initialInfo = [
-  {
+  {//첫번째 페이지
     info1: "",
     info2: "",
     info3: "",
@@ -12,7 +12,7 @@ const initialInfo = [
     date: "",
     info5: "",
     info6: []
-  },{
+  },{//두번째 페이지
     info1: "",
     info2: "",
     info3: "",
@@ -20,7 +20,7 @@ const initialInfo = [
     date: "",
     info5: "",
     info6: []
-  },{
+  },{//세번째 페이지
     info1: "",
     info2: "",
     info3: "",
@@ -28,7 +28,7 @@ const initialInfo = [
     date: "",
     info5: "",
     info6: []
-  },{
+  },{//네번째 페이지
     info1: "",
     info2: "",
     info3: "",
@@ -41,11 +41,19 @@ const initialInfo = [
 
 function App() {
 
+  //POST로 보낼 정보
   const [info, setInfo] = useState(initialInfo);
+
+  //현재 페이지
   const [page, setPage] = useState(0);
+
+  //메뉴 슬라이드 CK
   const [fold, setfold] = useState({first: false, second: false});
-  const [cal, setCal] = useState(true);
-  
+
+  //달력 활성화 CK
+  const [cal, setCal] = useState(false);
+
+  //정보 변경 시 setState
   const editInfoFnc = (e) => {
     let new_Info = [...info];
     const info_Type = e.target.name;
@@ -54,13 +62,13 @@ function App() {
         new_Info[page].info6.push(e.target.value);
       else
         new_Info[page].info6 = new_Info[page].info6.filter(val => val !== e.target.value);
-
     }else 
       new_Info[page] = {...new_Info[page], [info_Type] : e.target.value};
     
     setInfo(new_Info);
   };
 
+  //저장 버튼 클릭 시
   const saveInfoFnc = async(e) => {
     e.preventDefault();
     try {
@@ -69,11 +77,12 @@ function App() {
         alert(res.data.data.message);
       }  
     } catch (error) {
-      alert(error.response.data.error.message);
+      const err_Data = error.response.data;
+      alert(`${err_Data.error.message}\nERROR CODE : ${err_Data.httpcode}`);
     }
-    
   }
 
+  //정보 불러오기
   const getInfoFnc = async() => {
     try {
       const res = await axios.get('https://api-jobtest.json2bot.chat/test');
@@ -140,7 +149,14 @@ function App() {
             <h2>날짜</h2>
             <input className='date_input' type="text" name="date" onChange={editInfoFnc} value={info[page].date} placeholder="yyyy.mm.dd"
               onFocus={() => setCal(true)}/>
-            {cal && <Calendar info={info} setInfo={setInfo} page={page}/>}
+            {cal && (
+              <>
+              <Calendar info={info} setInfo={setInfo} page={page}/>
+              {/* 날짜 input, Calendar 외부 영역 클릭 시 달력 비활성화 */}
+              <div onClick={() => setCal(false)} style={{width: '100%', height: '100%', position: 'fixed', zIndex: '9', top: '0px', left: '0px'}}></div>
+              </>
+            )}
+            
           </div>
           <div className='info_div'>
             <h2>정보5</h2>
--- src/Calendar.js
+++ src/Calendar.js
@@ -2,30 +2,54 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from './css/Calendar.module.scss';
 
 const Calendar = ({ info, setInfo, page }) => {
-  const [days, setDays] = useState({last: [], this: [], next: []});//달력에 표시할 날짜 숫자 { last: [지난달], this: [이번달], next: [다음달] }
-  const [selectInfo, setSelectInfo] = useState({year: 0, month: 0, date: new Date(), isDate: false});//선택한 Year, Month, Date
 
-  const monthLoop = () => {//Month select option
+  //달력에 표시할 날짜 숫자 { last: [지난달], this: [이번달], next: [다음달] }
+  const [days, setDays] = useState({last: [], this: [], next: []});
+
+  //선택한 Year, Month, Date, isDate(입력한 날짜가 DATE 타입인 경우 true)
+  const [selectInfo, setSelectInfo] = useState({year: 0, month: 0, date: new Date(), isDate: false});
+
+  //년도 월 select Box option 보여주기 위해 CK
+  const [option, setoption] = useState({year: false, month: false});
+
+  //년도 ol Ref
+  const year_Ref = useRef(null);
+
+  //월 ol Ref
+  const month_Ref = useRef(null);
+
+  //Month option list
+  const monthLoop = () => {
     const month_Arr = [];
     for(let i=1; i<13; i++){
       month_Arr.push(
-        <option key={i} value={i} selected={selectInfo.month === (i-1) ? true : false}>{i}</option>
+        <li key={i} className={selectInfo.month === (i-1) ? styles.selected_option : styles.option}
+        onClick={() => {
+          setSelectInfo({...selectInfo, month: i-1});
+          setoption({year: false, month:false});
+        }}>{i}월</li>
       );
     }
     return month_Arr;
   }
 
-  const yearLoop = () => {//Year select option (100년전 부터 이번년도까지)
+  //Year option list (100년전 부터 이번년도까지)
+  const yearLoop = () => {
     const year_Arr = [];
     const max_Year = new Date().getFullYear();
-    for(let i=max_Year-100; i<=max_Year; i++){
+    for(let i=max_Year; i>=max_Year-100; i--){
       year_Arr.push(
-        <option key={i} value={i} selected={selectInfo.year === i ? true : false}>{i}</option>
+        <li key={i} className={selectInfo.year === i ? styles.selected_option : styles.option}
+        onClick={() => {
+          setSelectInfo({...selectInfo, year: i});
+          setoption({year: false, month:false});
+        }}>{i}년</li>
       );
     }
     return year_Arr;
   }
 
+  //달력날짜 클릭 -> info[page].date변경
   const changeDateFnc = (day) => {
     const new_Info = [...info];
     let new_Month = selectInfo.month+1;
@@ -56,6 +80,31 @@ const Calendar = ({ info, setInfo, page }) => {
       setSelectInfo({...selectInfo, month: new_Month+1});
   }
 
+  //select Box 클릭시
+  const selectOptionSetFnc = (type) => {
+    if(type === 'year'){
+      setoption({year: !option.year, month: false});
+      const max_Year = new Date().getFullYear();
+      let scroll_Y = (max_Year - selectInfo.date.getFullYear() - 3) * 28;
+      year_Ref.current.scrollTo(0, scroll_Y);
+    }else if(type === 'month'){
+      setoption({year: false, month: !option.month});
+    }
+    
+  }
+
+  //select Box 년도 월 option 처음에 보여줄 때 value scroll
+  useEffect(() => {
+    if(option.year){
+      const max_Year = new Date().getFullYear();
+      let scroll_Y = (max_Year - selectInfo.year - 3) * 28;
+      year_Ref.current.scrollTo(0, scroll_Y);
+    }else if(option.month){
+      let scroll_Y = (selectInfo.month - 3) * 28;
+      month_Ref.current.scrollTo(0, scroll_Y);
+    }
+  }, [option]);
+
   //달력의 년도 or 월 변경 될 때 달력 숫자 만들기
   useEffect(() => {
     const new_days = {last: [], this: [], next: []};//달력에 표시할 날짜 숫자
@@ -81,29 +130,46 @@ const Calendar = ({ info, setInfo, page }) => {
 
   },[selectInfo.year, selectInfo.month]);
 
-  //선택한 날짜가 변경 될 때
+  //날짜가 변경 될 때 (달력선택 or 입력수정)
   useEffect(() => {
     const info_Date = new Date(info[page].date);
+
+    //잘못된 값을 넣으면 달력은 오늘 날짜 기준으로 보여줌
     if(info_Date.toString() !== 'Invalid Date') {
       setSelectInfo({ year: info_Date.getFullYear(), month: info_Date.getMonth(), date: info_Date, isDate: true });
     }else {
       const curt_Date = new Date();
       setSelectInfo({ year: curt_Date.getFullYear(), month: curt_Date.getMonth(), date: curt_Date, isDate: false });
     }
+
+    //년도 월 select Box hidden
+    setoption({year: false, month: false});
   },[info[page].date]);
 
   return (
     <div className={styles.cal_wrap}>
       <div className={styles.cal_div}>
         <div className={styles.nav_div}>
-          <div onClick={moveLastMonthFnc}><span className={styles.left_btn}></span></div>
-          <select defaultValue={selectInfo.year} onChange={(e) => setSelectInfo({...selectInfo, year: Number(e.target.value)})}>
-            {yearLoop()}
-          </select>
-          <select defaultValue={selectInfo.month} onChange={(e) => setSelectInfo({...selectInfo, month: Number(e.target.value)-1})}>
-            {monthLoop()}
-          </select>
-          <div onClick={moveNextMonthFnc}><span className={styles.right_btn}></span></div>
+          <div className={styles.btn_div} onClick={moveLastMonthFnc}><span className={styles.left_btn}></span></div>
+          <div className={styles.select} id={option.year ? styles.select_filled : ''}>
+            <div className={styles.select_val} onClick={() => selectOptionSetFnc('year')}>
+              <p>{selectInfo.year}년</p>
+              <span className={styles.bottom_btn}></span>
+            </div>
+            <ol className={styles.option_list} ref={year_Ref} hidden={!option.year}>
+              {yearLoop()}
+            </ol>
+          </div>
+          <div className={styles.select} id={option.month ? styles.select_filled : ''}>
+            <div className={styles.select_val} onClick={() => selectOptionSetFnc('month')}>
+              <p>{selectInfo.month+1}월</p>
+              <span className={styles.bottom_btn}></span>
+            </div>
+            <ol className={styles.option_list} ref={month_Ref} hidden={!option.month}>
+              {monthLoop()}
+            </ol>
+          </div>
+          <div className={styles.btn_div} onClick={moveNextMonthFnc}><span className={styles.right_btn}></span></div>
         </div>
         <div className={styles.week_div}>
           <span>일</span>
@@ -115,7 +181,9 @@ const Calendar = ({ info, setInfo, page }) => {
           <span>토</span>
         </div>
         <div className={styles.days_div}>
+
           {days.last.map(day => {return <div onClick={() => setSelectInfo({...selectInfo, month: selectInfo.month-1})} key={day} className={styles.last_days}>{day}</div>})}
+
           {days.this.map(day => {
             let today = false; //today 체크
             let selectDay = false; //선택한 날짜 체크
@@ -131,7 +199,9 @@ const Calendar = ({ info, setInfo, page }) => {
             return <div onClick={() => changeDateFnc(day)} key={day} className={selectDay ? styles.select_day : styles.this_days}
               style={today ? {fontWeight: 'bold'} : {fontWeight: 'normal'}}>{day}</div>
           })}
+
           {days.next.map(day => {return <div onClick={() => setSelectInfo({...selectInfo, month: selectInfo.month+1})} key={day} className={styles.next_days}>{day}</div>})}
+
         </div>
       </div>
     </div>
